1573cd8408896ce970fbede0e640859624dfa8d6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-18 00:38:51 +0900

    refactor: api 호출 코드 리턴문 수정

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,21 +1,29 @@
 import axios from 'axios';
 
+import { CommentType } from '../interface';
+
+const getAllComments = () => axios.get('http://localhost:4000/comments');
+
 const getComments = (
   page: number,
   limit: number,
   order: string,
   sort: string
-) => {
+) =>
   axios.get(
     `http://localhost:4000/comments?_page=${page}&_limit=${limit}&_order=${order}&_sort=${sort}`
   );
-};
 
-const getComment = (id: number) => {
+const getComment = (id: number) =>
   axios.get(`http://localhost:4000/comments/${id}`);
-};
 
-const postComment = ({ id, profile_url, author, content, createdAt }: any) => {
+const postComment = ({
+  id,
+  profile_url,
+  author,
+  content,
+  createdAt,
+}: CommentType) =>
   axios.post('http://localhost:4000/comments', {
     id,
     profile_url,
@@ -23,22 +31,26 @@ const postComment = ({ id, profile_url, author, content, createdAt }: any) => {
     content,
     createdAt,
   });
-};
 
-const putComment = ({ id, profile_url, author, content, createdAt }: any) => {
+const putComment = ({
+  id,
+  profile_url,
+  author,
+  content,
+  createdAt,
+}: CommentType) =>
   axios.put(`http://localhost:4000/comments/${id}`, {
     profile_url,
     author,
     content,
     createdAt,
   });
-};
 
-const deleteComment = (id: number) => {
+const deleteComment = (id: number) =>
   axios.delete(`http://localhost:4000/comments/${id}`);
-};
 
 const CommentService = {
+  getAllComments,
   getComments,
   getComment,
   postComment,
