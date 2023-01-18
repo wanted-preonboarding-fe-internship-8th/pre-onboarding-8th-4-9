da25d99cae7222deb9c35b2e0a673cbf5ce17486
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-18 22:15:17 +0900

    feat: logger 붙이기

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+import { createLogger } from 'redux-logger';
 
 import { commentsReducer } from '../features/Comments/commentsSlice';
+
+const logger = createLogger();
 
 export const store = configureStore({
   reducer: {
     comments: commentsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
